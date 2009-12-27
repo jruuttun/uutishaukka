e9e112e29e77464053cb71f6c0f046cbe75361b9
jruuttun <jruuttun@81b928af-e9d2-49e9-aec1-7ddddc8443b9>
2009-12-27 11:32:09 +0000

    tulosteiden utf-8-enkoodaus, datahakemisto parametrina
    git-svn-id: svn+ssh://kapsi.fi/home/users/jruuttun/svn/uutishaukka@5 81b928af-e9d2-49e9-aec1-7ddddc8443b9

--- uutishaukka.py
+++ uutishaukka.py
@@ -20,64 +20,78 @@ from datetime import datetime
 def equal(oldVersion, newVersion):
     return oldVersion == newVersion
 
+# Komentoriviargumentit (sys.argv)
+import sys
 
 # Käy läpi annetussa rss-itemissä viitatun jutun ja tallentaa jutulle
 # uuden version, ellei kyseinen versio ole jo tallessa
-def processItem(channelTitle, itemElement):
+def processItem(channelDir, itemElement):
     itemTitle = itemElement.getElementsByTagName("title")[0].firstChild.data
-    itemDir = channelTitle + os.sep + itemTitle
+    itemDir = channelDir + os.sep + itemTitle
     latestVersionHtml = None
     print "**"
     if not os.path.exists(itemDir):
         os.mkdir(itemDir)
-        print "Uusi uutisjuttu: " + itemDir
+        print "Uusi uutisjuttu: " + itemDir.encode('utf-8')
     else:
         versionNames = os.listdir(itemDir)
         if len(versionNames) > 0: 
             versionNames.sort(); versionNames.reverse()
-            print "Uutisjutun '" + itemDir + "' viimeisin versio: " + versionNames[0]
+            print "Uutisjutun '" + itemDir.encode('utf-8') + "' viimeisin versio: " + versionNames[0].encode('utf-8')
             latestVersionFile = open(itemDir + os.sep + versionNames[0], 'r');
             latestVersionHtml = latestVersionFile.read()
             latestVersionFile.close()
         
     itemLink = itemElement.getElementsByTagName("link")[0].firstChild.data
     print "Haetaan juttu: " + itemLink
-    storyHtml = urllib2.urlopen(itemLink).read()
-    saveVersion = True
+    try:
+        storyHtml = urllib2.urlopen(itemLink).read()
+        saveVersion = True
+        
+        if latestVersionHtml is not None:
+            print "Vertaillaan juttua viimeisimpään versioon..."
+            if equal(latestVersionHtml, storyHtml):
+                saveVersion = False
+                print "Ei muutoksia edellisen ajon jalkeen"
+            else:
+                print "Juttu muuttunut, tallennetaan uusi versio"
+                
+                if saveVersion:
+                    storyFilename = datetime.now().isoformat() + ".html"
+                    storyPath = itemDir + os.sep + storyFilename
+                    if latestVersionHtml is None:
+                        print "Ensimmäinen versio:" + storyPath.encode('utf-8')
+                    else:
+                        print "Uusi versio: " + storyPath.encode('utf-8')
+                        storyFile = open(storyPath, 'w');
+                        storyFile.write(storyHtml);
+                        storyFile.close();
+    except urllib2.URLError, urlError:
+        print "Juttu ei ole saatavilla: ", urlError 
 
-    if latestVersionHtml is not None:
-        print "Vertaillaan juttua viimeisimpään versioon..."
-        if equal(latestVersionHtml, storyHtml):
-            saveVersion = False
-            print "Ei muutoksia edellisen ajon jalkeen"
-        else:
-            print "Juttu muuttunut, tallennetaan uusi versio"
-        
-    if saveVersion:
-        storyFilename = datetime.now().isoformat() + ".html"
-        storyPath = itemDir + os.sep + storyFilename
-        if latestVersionHtml is None:
-            print "Ensimmainen versio:" + storyPath
-        else:
-            print "Uusi versio: " + storyPath
-        storyFile = open(storyPath, 'w');
-        storyFile.write(storyHtml);
-        storyFile.close();
-        
 
 # Käy läpi syötteen jutut ja päivittää niiden tiedot syötedokumentin
 # elementin channel->title mukaisiin hakemistoihin
 # processItem-funktion avulla
-def processChannel(rssDocument):
+def processChannel(path, rssDocument):
     channelNode = rssDocument.getElementsByTagName("channel")[0]
     channelTitle = channelNode.getElementsByTagName("title")[0].firstChild.data
-    if not os.path.exists(channelTitle):
-        os.mkdir(channelTitle)
-        print "Uusi uutissyote: " + channelTitle
+    channelDir = path + os.sep + channelTitle
+    if not os.path.exists(channelDir):
+        os.mkdir(channelDir)
+        print "Tallennetaan uusi uutiskanava: " + channelDir
     itemElements = channelNode.getElementsByTagName("item");
     for itemElement in itemElements:
-        processItem(channelTitle, itemElement)
+        processItem(channelDir, itemElement)
 
+
+#### Pääohjelma #######
+newsPath = '.'
+if(len(sys.argv) > 1):
+    newsPath = sys.argv[1]
+    print "Uutiset viedään hakemistoon " + newsPath.encode('utf-8')
+else:
+    print "Uutiset tallennetaan työhakemistoon"
 
 
 # Luetaan sisään syötteet, joita halutaan seurata
@@ -88,10 +102,13 @@ feedfile.close()
 # käsitellään kaikki syötelistassa mainitut rss-syötteet
 for feed in feeds:
     print "*** Syote: " + feed
-    file = urllib2.urlopen(feed)
-    rssString = file.read()
-    rssDocument = minidom.parseString(rssString);
-    processChannel(rssDocument)
-    file.close()
-
+    try:
+        file = urllib2.urlopen(feed)
+        rssString = file.read()
+        rssDocument = minidom.parseString(rssString);
+        processChannel(newsPath, rssDocument)
+        file.close()
+    except urllib2.URLError:
+        print "Syöte ei saatavilla, jatketaan seuraavaan"
+ 
 
